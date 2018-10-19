894e70bf4eac72fc88618eef801c2454c7ec7ac5
KIM JONG IL <nijin39@gmail.com>
2018-10-20 00:21:29 +0900

    Apply HTTP

--- src/main/scala/com/tandem6/attraction/GajakoreaApp.scala
+++ src/main/scala/com/tandem6/attraction/GajakoreaApp.scala
@@ -2,8 +2,36 @@
 package com.tandem6.attraction
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import scala.io.StdIn
 
 object GajakoreaApp extends App {
 
-  val system:ActorSystem = ActorSystem("GAJAKOREA")
+  val config = ConfigFactory.load()
+  val host = config.getString("http.host")
+  val port = config.getInt("http.port")
+
+  implicit val system:ActorSystem = ActorSystem("GAJAKOREA")
+  implicit val executionContext = system.dispatcher
+  implicit val materializer = ActorMaterializer()
+
+  val route =
+    path("hello") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    }
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+  //val bindingFuture: Future[ServerBinding] = Http().bindAndHandle(api, host, port)
 }
